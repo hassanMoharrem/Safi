709d8d1d121ab24fe5c6aa6fba0d46657e994f21
hassanMoharrem <mhrm781@gmail.com>
2024-05-07 10:08:00 +0300

    hass

--- app/Http/Controllers/User/Auth/AuthController.php
+++ app/Http/Controllers/User/Auth/AuthController.php
@@ -152,6 +152,7 @@ class AuthController extends Controller
                 "status" => 200,
                 'success' => true,
                 "message" => " برجى تأكيد رقم الهاتف",
+                "user" => $user,
             ]);
 //        }catch (\Exception $e){
 //            return response()->json([
